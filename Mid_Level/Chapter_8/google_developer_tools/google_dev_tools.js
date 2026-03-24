3338d2248e9e190f991e6468e3a48d59b64cf6f7
Lucrethon <lucrecimich@gmail.com>
2026-03-24 19:31:26 -0400

    Chapter 8: Google Dev Tools

---
+++ Mid_Level/Chapter_8/google_developer_tools/google_dev_tools.js
@@ -0,0 +1,70 @@
+//--------------------- Herramientas de Desarrollo del navegador (Chrome) ----------------
+
+// ---------- Pestaña Elements ----------
+
+//copy JS path: document.querySelector("body > div > p")
+//copy selector:  body > div > h2
+//cpy outerHTML: <h2>Titulo</h2>
+//copy XFullPath:  /html/body/div/h2
+
+
+// Filtros y buscar por palabra claves
+
+// Modificar, Crear y eliminar etiquetas
+
+// Modificar propiedades de CSS
+
+// Event Listeners
+
+// Properties
+
+// Opciones para copiar
+
+// Seguir explorando
+
+// -------- Pestaña Resources --------
+// Ver y guardar archivos
+
+// Información ofrecida
+
+// Cambios en tiempo real
+
+// Snippets - Ejecutar código por fragmentos
+
+// --------------- Pestaña Networks ---------------
+// Interfaz
+
+// Sort By y Filtros de Busqueda
+
+// Limpiar Cookies y Caché
+
+// Importar y Exportar
+
+// 49) Pestaña Timeline
+// Uso y Grabación
+
+// Screenshots de la grabación
+
+// Controls
+
+// Overview
+
+// FPS, CPU y Net
+
+// Flame Chart
+
+// Details
+
+// Sugerencias de grabación
+
+// ------------- Pestaña Application ----------------
+// Application
+
+// Storage
+
+// Caché
+
+// Background Services
+
+// Frames
+
